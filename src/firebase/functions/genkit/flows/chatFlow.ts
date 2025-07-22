e50b32dbddfb7c758277da4bfa0b7dac4c58bb4a
Andrii Olkhovych <andrii.olkhovych@pm.me>
2025-07-22 23:16:50 +0200

    refactor: update chatFlow to stream responses with sendChunk function

--- src/firebase/functions/genkit/flows/chatFlow.ts
+++ src/firebase/functions/genkit/flows/chatFlow.ts
@@ -7,52 +7,55 @@ import { faqRetriever, knowledgeRetriever } from "../retrievers";
 import { adminTools } from "../tools";
 
 export const chatFlow = ai.defineFlow(
-	{
-		name: "chatFlow",
-		inputSchema: z.object({
-			messages: z.array(
-				z.object({
-					role: z.enum(["user", "model", "tool", "system"]),
-					content: z.array(z.object({ text: z.string() })),
-				}),
-			),
-		}),
-		outputSchema: z.string(),
-	},
-	async ({ messages }, { context }) => {
-		const query =
-			messages
-				.reverse()
-				.find(({ role }) => role === "user")
-				?.content.at(0)?.text ?? "";
+  {
+    name: "chatFlow",
+    inputSchema: z.object({
+      messages: z.array(
+        z.object({
+          role: z.enum(["user", "model", "tool", "system"]),
+          content: z.array(z.object({ text: z.string() })),
+        }),
+      ),
+    }),
+    outputSchema: z.string(),
+  },
+  async ({ messages }, { context, sendChunk }) => {
+    const query =
+      messages
+        .reverse()
+        .find(({ role }) => role === "user")
+        ?.content.at(0)?.text ?? "";
 
-		const [faqs, knowledge] = await Promise.all([
-			ai.retrieve({
-				retriever: faqRetriever,
-				query,
-			}),
-			ai.retrieve({
-				retriever: knowledgeRetriever,
-				query,
-			}),
-		]);
+    const [faqs, knowledge] = await Promise.all([
+      ai.retrieve({
+        retriever: faqRetriever,
+        query,
+      }),
+      ai.retrieve({
+        retriever: knowledgeRetriever,
+        query,
+      }),
+    ]);
 
-		const resources = await getExternalResources();
-		const tools = await getExternalTools();
+    const resources = await getExternalResources();
+    const tools = await getExternalTools();
 
-		const admin = isAdmin(context);
+    const admin = isAdmin(context);
 
-		const { text } = await systemPrompt(
-			{ query, role: admin ? "ADMIN" : "NORMAL USER" },
-			{
-				resources,
-				docs: [...faqs, ...knowledge],
-				tools: admin ? [...tools, ...adminTools] : [],
-				messages: messages,
-				context,
-			},
-		);
+    const { stream, response } = systemPrompt.stream(
+      { query, role: admin ? "ADMIN" : "NORMAL USER" },
+      {
+        resources,
+        docs: [...faqs, ...knowledge],
+        tools: admin ? [...tools, ...adminTools] : [],
+        messages: messages,
+        context,
+      });
 
-		return text;
-	},
+    for await (const chunk of stream) {
+      sendChunk(chunk);
+    }
+
+    return (await response).text;
+  },
 );
